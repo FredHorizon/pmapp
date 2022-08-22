548934bce0b7322c4e42fa1c6a2f565df4ee3a4a
FredHorizon <fredoliveira.sys@gmail.com>
2022-08-22 19:47:02 -0400

    :package: Configura a página About

---
+++ pages/about.js
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+
+import styles from '../styles/About.module.css';
+
+export default function About() {
+  return (
+    <div className={styles.main}>
+      <h1>Sobre o projeto</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos
+        corporis impedit doloremque vitae, iusto nemo harum laborum voluptatum
+        natus amet optio ab inventore repellat eum libero. Veritatis sed quam
+        dolorum!
+      </p>
+      <Image
+        src="/images/charizard.png"
+        width="300"
+        height="300"
+        alt="Charizard"
+      />
+    </div>
+  );
+}
