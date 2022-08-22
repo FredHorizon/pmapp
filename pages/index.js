ab7fecd6beecfb9443f1cd4c60c2671730a85255
FredHorizon <fredoliveira.sys@gmail.com>
2022-08-22 14:04:40 -0400

    Inicia o projeto PokeNext

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,9 @@
 import styles from '../styles/Home.module.css';
 
-import Head from 'next/head';
-import Image from 'next/image';
-
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Iníco</title>
-        <meta name="keywords" content="Camisas, Roupas" />
-        <meta name="description" content="As melhores camisas estão aqui" />
-      </Head>
-      <div>
-        <h1 className={styles.title}>App</h1>
-        <Image
-          src="/images/im.jpg"
-          width="300px"
-          height="300px"
-          alt="Iron Maiden"
-        />
-      </div>
-    </>
+    <div>
+      <h1>PokeNext</h1>
+    </div>
   );
 }
