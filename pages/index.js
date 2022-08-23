e4ec0b7a6ca9ff318effa243949ccfe22d12f437
FredHorizon <fredoliveira.sys@gmail.com>
2022-08-22 20:03:01 -0400

    :package: Configura o consumo da API

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
 import styles from '../styles/Home.module.css';
 
-export default function Home() {
+export async function getStaticProps() {
+  const maxPokemons = 251;
+  const api = 'https://pokeapi.co/api/v2/pokemon/';
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`);
+  const data = await res.json();
+
+  // Cria índice para cada pokemon
+  data.results.forEach((item, index) => {
+    item.id = index + 1;
+  });
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  };
+}
+
+export default function Home({ pokemons }) {
   return (
     <div>
-      <h1>Index</h1>
+      <h1>PokeNext</h1>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
     </div>
   );
 }
