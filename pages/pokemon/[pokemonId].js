32a5d1573c23325dec8e9d453c88b9746b5a93dc
FredHorizon <fredoliveira.sys@gmail.com>
2022-08-23 16:29:33 -0400

    :package: Configura a página dinâmica de detalhes

---
+++ pages/pokemon/[pokemonId].js
@@ -0,0 +1,39 @@
+export const getStaticPaths = async () => {
+  const maxPokemons = 251;
+  const api = 'https://pokeapi.co/api/v2/pokemon/';
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`);
+  const data = await res.json();
+
+  // pegar os parametros
+  const paths = data.results.map((pokemon, index) => {
+    return {
+      params: { pokemonId: (index + 1).toString() },
+    };
+  });
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async (context) => {
+  const id = context.params.pokemonId;
+
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+
+  const data = await res.json();
+
+  return {
+    props: { pokemon: data },
+  };
+};
+
+export default function Pokemon({ pokemon }) {
+  return (
+    <div>
+      <h1>{pokemon.name}</h1>
+    </div>
+  );
+}
