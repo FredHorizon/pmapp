3c6d56600c9eb1aaf19fe2adace879fd955eff48
FredHorizon <fredoliveira.sys@gmail.com>
2022-08-26 13:16:42 -0400

    :package: Projeto buildado para deploy (v1)

--- pages/about.js
+++ pages/about.js
@@ -7,9 +7,10 @@ export default function About() {
     <div className={styles.main}>
       <h1>Sobre o projeto</h1>
       <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos
-        corporis impedit doloremque vitae, iusto nemo harum laborum voluptatum
-        natus amet optio ab inventore repellat eum libero.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Beatae
+        corporis possimus nisi minus hic, voluptatum reiciendis! Distinctio
+        quod, vel ratione qui alias modi debitis placeat ipsa reiciendis hic
+        deserunt aliquam.
       </p>
       <Image
         src="/images/charizard.png"
