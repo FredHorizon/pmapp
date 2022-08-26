49768fd34236480c6ff6878db2f8f93d2b264612
FredHorizon <fredoliveira.sys@gmail.com>
2022-08-26 14:21:16 -0400

    :package: Implementa o carregamento de dados não renderizados

--- pages/pokemon/[pokemonId].js
+++ pages/pokemon/[pokemonId].js
@@ -2,6 +2,8 @@ import Image from 'next/image';
 
 import styles from '../../styles/Pokemon.module.css';
 
+import { useRouter } from 'next/router';
+
 export const getStaticPaths = async () => {
   const maxPokemons = 251;
   const api = 'https://pokeapi.co/api/v2/pokemon/';
@@ -18,7 +20,7 @@ export const getStaticPaths = async () => {
 
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
 
@@ -35,6 +37,12 @@ export const getStaticProps = async (context) => {
 };
 
 export default function Pokemon({ pokemon }) {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Carregando...</div>;
+  }
+
   return (
     <div className={styles.pokemon_container}>
       <h1 className={styles.pokemon_title}>{pokemon.name}</h1>
